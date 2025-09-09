8537979af4e0c263ad9cc0ad874910ca40677337
Thrisha Mae Martizano <martizanothrisha@gmail.com>
2025-09-09 09:57:24 +0800

    Added Student Variables

--- MartizanoLife.js
+++ MartizanoLife.js
@@ -1,11 +1,10 @@
 console.log("IT21C");
 
-// let student = "Thrisha Mae Martizano";
-// let course = "Bachelor of Science in Information Technology";
-// let yearlevel = "2nd Year";
+let student = "Thrisha Mae Martizano";
+let course = "Bachelor of Science in Information Technology";
+let yearlevel = "2nd Year";
 
-// console.log("Name: " +student+ "\nCourse: " +course+ "\nYear Level: " +yearlevel+ "\n" );
+console.log("Name: " +student+ "\nCourse: " +course+ "\nYear Level: " +yearlevel+ "\n" );
 
-let hours1 = 8;
-let hours2 = 2;
+
 
