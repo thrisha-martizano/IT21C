0d2f27cfa25c7b7345c61de934d318d591efcc8b
Thrisha Mae Martizano <martizanothrisha@gmail.com>
2025-09-09 10:13:50 +0800

    Added Operators for Daily Routine

--- MartizanoLife.js
+++ MartizanoLife.js
@@ -1,10 +1,19 @@
 console.log("IT21C");
 
-let student = "Thrisha Mae Martizano";
-let course = "Bachelor of Science in Information Technology";
-let yearlevel = "2nd Year";
+// let student = "Thrisha Mae Martizano";
+// let course = "Bachelor of Science in Information Technology";
+// let yearlevel = "2nd Year";
 
-console.log("Name: " +student+ "\nCourse: " +course+ "\nYear Level: " +yearlevel+ "\n" );
+// console.log("Name: " +student+ "\nCourse: " +course+ "\nYear Level: " +yearlevel+ "\n" );
+
+let hours1 = 5;
+let hours2 = 3;
+
+let sum = hours1+hours2;
+let diff = hours1-hours2;
+
+console.log("Total Hours of Sleep: " + sum + "\nTotal Hours of Study: " + diff + "\n");
+
 
 
 
