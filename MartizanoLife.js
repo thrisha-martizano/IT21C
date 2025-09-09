0b0086af1b69695f7c57728eb278e57063ddc693
Thrisha Mae Martizano <martizanothrisha@gmail.com>
2025-09-09 09:39:21 +0800

    Added Student Variables

---
+++ MartizanoLife.js
@@ -0,0 +1,7 @@
+console.log("IT21C");
+
+let student = "Thrisha Mae Martizano";
+let course = "Bachelor of Science in Information Technology";
+let yearlevel = "2nd Year"
+
+console.log("Name: " +student+ "\nCourse: " +course+ "\nYear Level: " +yearlevel+ "\n" );
---
+++ index.js
@@ -0,0 +1,56 @@
+/*console.log("Hello World");
+
+let student = "Thrisha Mae Martizano";
+let grade = 1.0;
+let isPassed = true;
+let address;
+let partner = null;
+
+console.log("Student Name: "+student+ "\nFinal Grade: " +grade + "\nPassed?:" +isPassed+ "\nAddress: " +address+ "\nPartner: " +partner+ "\n" );
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1+num2;
+let diff = num1-num2;
+let prod = num1*num2;
+let quo = num1/num2;
+let mod = num1%num2;
+
+console.log("Sum: " + sum + "\nDifference: " + diff + "\nProduct: " + prod + "\nQuotient: " + quo + "\nReminder: " + mod + "\n" );
+
+
+let grade = 70;
+
+//Create conditional Statement
+
+if (grade > 75){
+    console.log("Students Passed!");
+}else {
+    console.log("Students Failed!");
+
+}
+
+//Loop
+
+for(let i = 0; i<=5; i++){
+    console.log("Count: " + i);
+}
+
+// While Loop
+
+let i = 0;
+
+while(i<=5){
+    console.log("Count: " + i);
+    i++;
+}
+*/
+
+//Function
+
+function greet(name){
+    console.log("Hello, " + name );
+}
+
+greet("Thrisha");
