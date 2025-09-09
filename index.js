1e9836d48bf3ab206f9dd76b81c931a049f42e79
Thrisha Mae Martizano <martizanothrisha@gmail.com>
2025-09-09 16:48:34 +0800

    Image SS

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-/*console.log("Hello World");
+// console.log("Hello World");
 
-let student = "Thrisha Mae Martizano";
-let grade = 1.0;
-let isPassed = true;
-let address;
-let partner = null;
+// let student = "Thrisha Mae Martizano";
+// let grade = 1.0;
+// let isPassed = true;
+// let address;
+// let partner = null;
 
-console.log("Student Name: "+student+ "\nFinal Grade: " +grade + "\nPassed?:" +isPassed+ "\nAddress: " +address+ "\nPartner: " +partner+ "\n" );
+// console.log("Student Name: "+student+ "\nFinal Grade: " +grade + "\nPassed?:" +isPassed+ "\nAddress: " +address+ "\nPartner: " +partner+ "\n" );
 
-let num1 = 10;
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
-let sum = num1+num2;
-let diff = num1-num2;
-let prod = num1*num2;
-let quo = num1/num2;
-let mod = num1%num2;
+// let sum = num1+num2;
+// let diff = num1-num2;
+// let prod = num1*num2;
+// let quo = num1/num2;
+// let mod = num1%num2;
 
-console.log("Sum: " + sum + "\nDifference: " + diff + "\nProduct: " + prod + "\nQuotient: " + quo + "\nReminder: " + mod + "\n" );
+// console.log("Sum: " + sum + "\nDifference: " + diff + "\nProduct: " + prod + "\nQuotient: " + quo + "\nReminder: " + mod + "\n" );
 
 
-let grade = 70;
+// let grade = 70;
 
-//Create conditional Statement
+// //Create conditional Statement
 
-if (grade > 75){
-    console.log("Students Passed!");
-}else {
-    console.log("Students Failed!");
+// if (grade > 75){
+//     console.log("Students Passed!");
+// }else {
+//     console.log("Students Failed!");
 
-}
+// }
 
-//Loop
+// //Loop
 
-for(let i = 0; i<=5; i++){
-    console.log("Count: " + i);
-}
+// for(let i = 0; i<=5; i++){
+//     console.log("Count: " + i);
+// }
 
-// While Loop
+// // While Loop
 
-let i = 0;
+// let i = 0;
 
-while(i<=5){
-    console.log("Count: " + i);
-    i++;
-}
-*/
+// while(i<=5){
+//     console.log("Count: " + i);
+//     i++;
+// }
 
-//Function
+
+// //Function
 
 function greet(name){
     console.log("Hello, " + name );
 }
 
-greet("Thrisha");
+// greet("Thrisha");
